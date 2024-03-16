8edbf3a61dddb273a96215f857b6bc9a1ccd9a58
Sardor Narzikulov <sardornarzikulovv2002@gmail.com>
2024-03-16 17:01:56 +0500

    month change

--- script.js
+++ script.js
@@ -4,24 +4,50 @@ const modalPlaceholder = document.getElementById("matn");
 const saveModal = document.getElementById("save");
 const timelineBox = document.getElementById("timeline-card");
 
-// saveModal.addEventListener("click")
 
-function createTimeline() {
-    let title = document.createElement("h3");
-    title.innerHTML = modalTitle.value;
-    let placeholder = document.createElement("p");
-    placeholder.innerHTML = modalPlaceholder.value;
+   
+    function createTimeline() {
 
-    let timelineCard = document.createElement("div");
-    timelineCard.classList.add("timeline-card");
+        if(modalTitle.value == ""){
+            console.log("Bosh");
+            saveModal.removeAttribute("data-bs-dismiss")
+        }else{
+            let title = document.createElement("h3");
+            title.innerHTML = modalTitle.value;
+            let placeholder = document.createElement("p");
+            placeholder.innerHTML = modalPlaceholder.value;
+        
+            let timelineCard = document.createElement("div");
+            timelineCard.classList.add("timeline-card");
+        
+            let timelineDot = document.createElement("span");
+            timelineDot.classList.add("timeline-dot");
+            // month create
+            const monthNames = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"];
+            const nowTime = new Date();
+            
+            const month = nowTime.getMonth();
 
-    let timelineDot = document.createElement("span");
-    timelineDot.classList.add("timeline-dot");
-    // create card
-    timelineCard.appendChild(timelineDot);
-    timelineCard.appendChild(title);
-    timelineCard.appendChild(placeholder);
+            let time = document.createElement("span");
+            time.classList.add("timeline-time");
+            time.innerHTML = `${nowTime.getDay} + . + ${monthNames[month]} + ${nowTime.getFullYear}`;
 
-    // add card
-    timelineBox.appendChild(timelineCard);
-}
+            // create card
+            timelineCard.appendChild(timelineDot);
+            timelineCard.appendChild(title);
+            timelineCard.appendChild(placeholder);
+            timelineCard.appendChild(time);
+        
+            // add card
+            timelineBox.appendChild(timelineCard);
+            
+            saveModal.setAttribute("data-bs-dismiss", "modal");
+            
+        }
+
+        modalTitle.value = "";
+        modalPlaceholder.value = "";
+  
+    }
+
+
