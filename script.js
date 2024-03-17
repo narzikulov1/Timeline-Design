5bd2433631ded8abb92a6e5b7e1319146735b190
Sardor Narzikulov <sardornarzikulovv2002@gmail.com>
2024-03-17 12:19:33 +0500

    I corrected the date

--- script.js
+++ script.js
@@ -5,49 +5,51 @@ const saveModal = document.getElementById("save");
 const timelineBox = document.getElementById("timeline-card");
 
 
-   
-    function createTimeline() {
 
-        if(modalTitle.value == ""){
-            console.log("Bosh");
-            saveModal.removeAttribute("data-bs-dismiss")
-        }else{
-            let title = document.createElement("h3");
-            title.innerHTML = modalTitle.value;
-            let placeholder = document.createElement("p");
-            placeholder.innerHTML = modalPlaceholder.value;
-        
-            let timelineCard = document.createElement("div");
-            timelineCard.classList.add("timeline-card");
-        
-            let timelineDot = document.createElement("span");
-            timelineDot.classList.add("timeline-dot");
-            // month create
-            const monthNames = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"];
-            const nowTime = new Date();
-            
-            const month = nowTime.getMonth();
+function createTimeline() {
 
-            let time = document.createElement("span");
-            time.classList.add("timeline-time");
-            time.innerHTML = `${nowTime.getDay} + . + ${monthNames[month]} + ${nowTime.getFullYear}`;
+    if (modalTitle.value == "") {
+        console.log("Bosh");
+        saveModal.removeAttribute("data-bs-dismiss")
+    } else {
+        let title = document.createElement("h3");
+        title.innerHTML = modalTitle.value;
+        let placeholder = document.createElement("p");
+        placeholder.innerHTML = modalPlaceholder.value;
 
-            // create card
-            timelineCard.appendChild(timelineDot);
-            timelineCard.appendChild(title);
-            timelineCard.appendChild(placeholder);
-            timelineCard.appendChild(time);
-        
-            // add card
-            timelineBox.appendChild(timelineCard);
-            
-            saveModal.setAttribute("data-bs-dismiss", "modal");
-            
-        }
+        let timelineCard = document.createElement("div");
+        timelineCard.classList.add("timeline-card");
 
-        modalTitle.value = "";
-        modalPlaceholder.value = "";
-  
+        let timelineDot = document.createElement("span");
+        timelineDot.classList.add("timeline-dot");
+        // month create
+        const monthNames = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"];
+        const nowTime = new Date();
+
+        const month = nowTime.getMonth();
+
+        console.log(nowTime.getDate());
+
+        let time = document.createElement("span");
+        time.classList.add("timeline-time");
+        time.innerHTML = `${nowTime.getDate()}. ${monthNames[month]}  ${nowTime.getFullYear()}`;
+
+        // create card
+        timelineCard.appendChild(timelineDot);
+        timelineCard.appendChild(title);
+        timelineCard.appendChild(placeholder);
+        timelineCard.appendChild(time);
+
+        // add card
+        timelineBox.appendChild(timelineCard);
+
+        saveModal.setAttribute("data-bs-dismiss", "modal");
+
     }
+
+    modalTitle.value = "";
+    modalPlaceholder.value = "";
+
+}
 
 
